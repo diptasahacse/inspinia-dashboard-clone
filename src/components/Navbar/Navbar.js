fac14a5d9b505dd6a267ede28b91dcd1bfb5cf0e
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-07-20 00:58:02 +0600

    replace some Link tag instead of a tag in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav class="navbar-default navbar-static-side" role="navigation">
@@ -7,17 +7,21 @@ const Navbar = () => {
                 <ul class="nav metismenu" id="side-menu">
                     <li class="nav-header">
                         <div class="dropdown profile-element">
-                            <img alt="image" class="rounded-circle" src="img/profile_small.jpg" />
+                            <img alt="image" class="rounded-circle" src="../../../public/assets/img/profile_small.jpg" />
                             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                                 <span class="block m-t-xs font-bold">David Williams</span>
                                 <span class="text-muted text-xs block">Art Director <b class="caret"></b></span>
                             </a>
                             <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                <li><a class="dropdown-item" href="profile.html">Profile</a></li>
-                                <li><a class="dropdown-item" href="contacts.html">Contacts</a></li>
+                                <li><Link className='dropdown-item' to='/profile'>Profile</Link></li>
+                                <li><Link className='dropdown-item' to='/contacts'>Contacts</Link></li>
+                                <li><Link className='dropdown-item' to='/mailbox'>Mailbox</Link></li>
+                                <li><Link className='dropdown-item' to='/login'>Logout</Link></li>
+                                {/* <li><a class="dropdown-item" href="profile.html">Profile</a></li> */}
+                                {/* <li><a class="dropdown-item" href="contacts.html">Contacts</a></li>
                                 <li><a class="dropdown-item" href="mailbox.html">Mailbox</a></li>
                                 <li class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="login.html">Logout</a></li>
+                                <li><a class="dropdown-item" href="login.html">Logout</a></li> */}
                             </ul>
                         </div>
                         <div class="logo-element">
